af1d5d36bd68d200b37f1c4cf1145a60ca3190b8
Baktyiar <Baktyiar1>
2024-10-02 20:46:08 +0600

    .env

--- core/core/settings.py
+++ core/core/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 
 from decouple import config
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,10 +31,13 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-
+#
 ALLOWED_HOSTS = ['*']
 
-
+# print("Available Environment Variables:")
+# for key in os.environ.keys():
+#     print(key)
+# print("SECRET_KEY:", os.getenv('SECRET_KEY'))
 # Application definition
 
 INSTALLED_APPS = [
